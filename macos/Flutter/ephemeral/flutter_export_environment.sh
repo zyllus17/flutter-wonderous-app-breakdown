9d7089d0d34cd8d0ede5e943c3e57869e2aba18b
zyllus17 <mrfhassan17@gmail.com>
2023-03-18 00:21:17 +0530

    Autogenerated code

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\_dev\sdks\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\_dev\gskinner\flutter_wonders_app"
+export "FLUTTER_ROOT=/Users/marufhassan/fvm/versions/3.7.3"
+export "FLUTTER_APPLICATION_PATH=/Users/marufhassan/personal/flutter-wonderous-app-breakdown"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.0.11"
-export "FLUTTER_BUILD_NUMBER=2.0.11"
+export "FLUTTER_BUILD_NAME=2.0.12"
+export "FLUTTER_BUILD_NUMBER=2.0.12"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
