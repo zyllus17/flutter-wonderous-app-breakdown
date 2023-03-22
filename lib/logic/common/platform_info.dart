aacf6b14795ca4ca7a4b68d03ac5e6e1d02f6b4f
zyllus17 <mrfhassan17@gmail.com>
2023-03-22 12:34:01 +0530

    Added documentation to code

--- lib/logic/common/platform_info.dart
+++ lib/logic/common/platform_info.dart
@@ -5,21 +5,24 @@ import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:wonders/common_libs.dart';
 
 class PlatformInfo {
+  // Define a list of desktop platforms
   static const _desktopPlatforms = [TargetPlatform.macOS, TargetPlatform.windows, TargetPlatform.linux];
+  // Define a list of mobile platforms
   static const _mobilePlatforms = [TargetPlatform.android, TargetPlatform.iOS];
-
+  // Checks for current platform
   static bool get isDesktop => _desktopPlatforms.contains(defaultTargetPlatform) && !kIsWeb;
   static bool get isDesktopOrWeb => isDesktop || kIsWeb;
   static bool get isMobile => _mobilePlatforms.contains(defaultTargetPlatform) && !kIsWeb;
-
+  // Define a static getter to get the pixel ratio of the device's screen
   static double get pixelRatio => WidgetsBinding.instance.window.devicePixelRatio;
-
+  // Define static getters for each platform type
   static bool get isWindows => defaultTargetPlatform == TargetPlatform.windows;
   static bool get isLinux => defaultTargetPlatform == TargetPlatform.linux;
   static bool get isMacOS => defaultTargetPlatform == TargetPlatform.macOS;
   static bool get isAndroid => defaultTargetPlatform == TargetPlatform.android;
   static bool get isIOS => defaultTargetPlatform == TargetPlatform.iOS;
-
+  // Define a static getter to check if the device is connected to the internet
   static Future<bool> get isConnected async => await InternetConnectionChecker().hasConnection;
+  // Define a static getter to check if the device is disconnected to the internet
   static Future<bool> get isDisconnected async => (await isConnected) == false;
 }
